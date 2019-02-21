16381948c9ccf2451003f8d534aa46f8e3fddc47
cameron <cameron@cmcgarry.co.uk>
2019-02-21 15:39:09 +0000

    Git ignore, setup script and readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# PhDWrite
+
+Various scribblings from across my PhD
+
+Ensure that uncommited files (e.g. \*.bib) are stored (or linked to) in
+`~/texmf/tex/latex`, or other relevant directories. This can be achieved by
+running `setup.sh`.
---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bib/bash
+
+# Check if latex directory exists and if not create it
+LATEXDIR=~/texmf/tex/latex
+if [ ! -d  $LATEXDIR ]; then
+  mkdir -p $LATEXDIR
+fi
+
+BIBTEXDIR=~/texmf/bibtex/bib
+if [ ! -d  $BIBTEXDIR ]; then
+  mkdir -p $BIBTEXDIR
+fi
+
+# Check if the bib file exists in the tex directory and if not then link it
+if [ ! -f $BIBTEXDIR/bib.bib ]; then 
+  read -e -p "Enter .bib file path: " BIBPATH
+  BIBPATH="${BIBPATH//\~/$HOME}"
+  ln $BIBPATH $BIBTEXDIR/bib.bib
+fi
+
+echo "Setup complete!"
